e6d5c7e6423305cc9811aba7d899cfffcba023ba
jaswanth chowdary <jassumeka@gmail.com>
2024-12-31 13:23:17 +0530

    changes made 4

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,21 @@
 import React from "react";
 
-export default function TodoList({ todos, onComplete }) {
+function TodoList({ todos, onComplete }) {
   return (
-    <>
-      <div>
-        <h2>Child Component</h2>
-        <ul>
-          {todos.map((todo) =>
-            todo.complete ? (
-              <li key={todo.id}>{todo.text}</li>
-            ) : (
-              <li key={todo.id}>
-                {todo.text}
-                <button onClick={() => onComplete(todo.id)}>complete</button>
-              </li>
-            )
-          )}
-        </ul>
-      </div>
-    </>
+    <div>
+      <h2>Child Component</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            {!todo.complete && (
+              <button onClick={() => onComplete(todo.id)}>Complete</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
+
+export default TodoList;
